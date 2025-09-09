3ca5a29e50e6fa364698d9989a69e580b8ff1f65
Jose Marinez <josems0899@gmail.com>
2025-09-08 20:51:44 -0600

    build changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import DashboardSidebar from '@/components/shared/DashboardSidebar';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 
 interface LayoutProps {
@@ -8,17 +10,26 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
     return (
-        <>
-            <div className="flex flex-row relative min-h-screen bg-gray-100">
-                <DashboardSidebar />
-                <div className="flex flex-1">
-                    <main className="flex-1 pt-6">
-                        <div className="">
-                            {children}
+        <ClerkProvider>
+            <>
+                <SignedIn>
+
+                    <div className="flex flex-row relative min-h-screen bg-gray-100">
+                        <DashboardSidebar />
+                        <div className="flex flex-1">
+                            <main className="flex-1 pt-6">
+                                <div className="">
+                                    {children}
+                                </div>
+                            </main>
                         </div>
-                    </main>
-                </div>
-            </div>
-        </>
+                    </div>
+                </SignedIn>
+                <SignedOut>
+                    {redirect('/')}
+                </SignedOut>
+            </>
+        </ClerkProvider>
+
     );
 }
