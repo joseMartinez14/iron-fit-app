b4f58ae07b47bd199d0c1ed28b4cad55d3d06c9f
Jose Marinez <josems0899@gmail.com>
2025-09-09 17:38:47 -0600

    Auth prod client

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ const isProtectedRoute = createRouteMatcher(["/api/protected(.*)"]);
 const defaultAllowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://iron-fit-client.netlify.app/",
 ];
 
 function getAllowedOrigins(): string[] {
